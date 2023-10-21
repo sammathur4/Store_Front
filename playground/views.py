7e4551b46c2001bfc0c209ad5954fc8dcecc4643
SAKSHAM <sammathur4@gmail.com>
2023-10-21 21:58:26 +0530

    Commit 7 : commit_message_index.txt: git_update.py: playground/__pycache__/views.cpython-311.pyc: playground/views.py

--- playground/views.py
+++ playground/views.py
@@ -21,7 +21,7 @@ def say_hello(request):
     # query_set = Product.objects.filter(Q(inventory__lt =10) | Q(price__lt=20))
     # query_set = Product.objects.order_by('price','-title')
     query_set = Product.objects.all()
-
+    #
     return render(request,
                   'hello.html',
                   {
