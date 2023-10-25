1c356ea59ca450ce8443a76da28a15d97597ccca
SAKSHAM <sammathur4@gmail.com>
2023-10-25 15:14:06 +0530

    Commit 11: commit_message_index.txt: playground/__pycache__/views.cpython-311.pyc: playground/views.py: templates/hello.html

--- playground/views.py
+++ playground/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 import store.models
-
-
+from django.db.models.aggregates import *
 
 Product = store.models.Product
 OrderItem = store.models.OrderItem
@@ -29,11 +28,16 @@ def say_hello(request):
     # query_set = Product.objects.prefetch_related('promotions').select_related('collection').all()
 
     # query_set = Order.objects.select_related('customer').order_by('-placed_at')[:5]
-    query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    # query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+
+    # result = Product.objects.aggregate(count=Count('id'),
+    #                                    min_price = Min('price'))
+    result = Product.objects.filter(collection__id = 1).aggregate(count=Count('id'))
     return render(request,
                   'hello.html',
                   {
                       'name': 'Sam',
-                      'products': list(query_set)
+                      'result': result,
+                      # 'products': list(query_set)
                   }
                   )
