0a7db9d2e9911583d0faac6077627009fe51765d
SAKSHAM <sammathur4@gmail.com>
2023-10-28 23:08:37 +0530

    Commit 14: storefront/storefront: storefront2/commit_message_index.txt: storefront2/store/__pycache__/urls.cpython-311.pyc: storefront2/store/__pycache__/views.cpython-311.pyc: storefront2/store/views.py

--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -10,31 +10,15 @@ from .serializers import *
 from rest_framework.views import *
 from rest_framework.mixins import *
 from rest_framework.generics import *
+from rest_framework.viewsets import ModelViewSet
 
 
-class ProductList(ListCreateAPIView):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.select_related('collection').all()
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         return {'request': self.request}
-
-    # def get(self, request):
-    #     queryset = Product.objects.select_related('collection').all()
-    #     serializer = ProductSerializer(queryset, many=True, context={'request': request})
-    #     return Response(serializer.data)
-    #
-    # def post(self, request):
-    #     serializer = ProductSerializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     print(serializer.validated_data)
-    #     return Response('ok')
-
-
-class ProductDetails(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
 
     def delete(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
@@ -58,7 +42,7 @@ class CollectionDetail(RetrieveUpdateDestroyAPIView):
     )
     serializer_class = CollectionsSerializer
 
-    def delete(self,request, pk):
+    def delete(self, request, pk):
         collection = get_object_or_404(
             Collection.objects.annotate(
                 products_count=Count('product')), pk=pk)
@@ -68,24 +52,3 @@ class CollectionDetail(RetrieveUpdateDestroyAPIView):
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def collection_details(request, pk):
-#     collection = get_object_or_404(
-#         Collection.objects.annotate(
-#             products_count=Count('product')), pk=pk)
-#
-#     if request.method == 'GET':
-#         serializer = CollectionsSerializer(collection)
-#         return Response(serializer.data)
-#     elif request.method == 'PUT':
-#         serializer = CollectionsSerializer(collection, data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data)
-#     elif request.method == 'DELETE':
-#         if collection.product_set.count() > 0:
-#             return Response({'error': 'Collection cannot be deleted because it includes one or more products.'},
-#                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
-#         collection.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
