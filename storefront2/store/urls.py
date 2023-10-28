eb08ad331d327c4d83a08602f23729dcf593ecf6
SAKSHAM <sammathur4@gmail.com>
2023-10-28 23:15:10 +0530

    Commit 16: storefront/storefront: storefront2/commit_message_index.txt: storefront2/store/__pycache__/urls.cpython-311.pyc: storefront2/store/__pycache__/views.cpython-311.pyc: storefront2/store/urls.py

--- storefront2/store/urls.py
+++ storefront2/store/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import SimpleRouter
+from pprint import pprint
+
+router = SimpleRouter()
+
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+pprint(router.urls)
 
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetails.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
-]
+urlpattern = router.urls
