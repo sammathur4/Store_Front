725fc8bbc69230d4b9ec762efb73ddea1285be02
SAKSHAM <sammathur4@gmail.com>
2023-10-30 13:46:11 +0530

    Commit 22: storefront/storefront: storefront2/commit_message_index.txt: storefront2/store/__pycache__/views.cpython-311.pyc: storefront2/store/views.py

--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -14,18 +14,31 @@ from rest_framework.viewsets import ModelViewSet
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.select_related('collection').all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        collection_id = self.request.query_params['collection_id']
+        if collection_id is not None:
+            queryset = queryset.filter(collection_id=collection_id)
+
+        return queryset
+
+    # filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    # filterset_class = ProductFilter
+    # pagination_class = DefaultPagination
+    # permission_classes = [IsAdminOrReadOnly]
+    # search_fields = ['title', 'description']
+    # ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
 
     def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
-            return Response({'error': 'Product cannot be deleted because it is associated with an order item.'},
-                            status=status.HTTP_405_METHOD_NOT_ALLOWED)
+            return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-        return super().destory(request, *args, **kwargs)
+        return super().destroy(request, *args, **kwargs)
 
 
 class CollectionViewSet(ModelViewSet):
@@ -44,12 +57,9 @@ class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
 
     def get_queryset(self):
-        print(("self"), self.kwargs)
         product_id = self.kwargs['product_pk']
-        teset =Review.objects.filter(product_id=product_id)
-        print("teset",teset)
-        return teset
-        # return Review.objects.filter(product_id=self.kwargs['product_pk'])
+        return Review.objects.filter(product_id=product_id)
+
 
     def get_serializer_context(self):
         print("self.kwargs['product_pk']", self.kwargs['product_pk'])
