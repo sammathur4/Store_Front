80535511d085f18210173fdee8262b63b85c9870
SAKSHAM <sammathur4@gmail.com>
2023-11-02 13:37:54 +0530

    Commit 28: storefront/storefront: storefront2/commit_message_index.txt: storefront2/main.py: storefront2/store/__pycache__/serializers.cpython-311.pyc

--- storefront2/main.py
+++ storefront2/main.py
@@ -3,11 +3,12 @@ To get jwt tokens:
 1. http://127.0.0.1:8000/auth/jwt/create
 
 
-"""
-
-
 
 {
     "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY5ODk5NDcxOSwiaWF0IjoxNjk4OTA4MzE5LCJqdGkiOiIxOWYzZmJmM2M5YTM0MDE1ODEyYjRhODdhMjk0NTQ0OSIsInVzZXJfaWQiOjc1fQ.FIlcP-7XzyzD9IOXBnu8_cvg9sLHl564LkKXGrK4xyo",
     "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk4OTk0NzE5LCJpYXQiOjE2OTg5MDgzMTksImp0aSI6IjllNzY2NWMxZDI3OTRhMWQ4ZWQxYjIzNTVmOTMwYjg4IiwidXNlcl9pZCI6NzV9.uSClMvV35U9-C5AOmwReHwUQvGoRL-xKAOxz70ydzOw"
 }
+
+JWT {token}
+
+"""
